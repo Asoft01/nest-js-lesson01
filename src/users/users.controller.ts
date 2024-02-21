c10b28968c342c18ba25dfeda2260b4d9a6a9859
Adeleke Hammed Lekan <lekhad19@gmail.com>
2024-02-21 20:08:43 +0100

    first commit for nest js

---
+++ src/users/users.controller.ts
@@ -0,0 +1,19 @@
+import { Controller, Get, Param } from '@nestjs/common';
+
+@Controller('users') 
+export class UsersController {
+    @Get() // GET /users 
+    findAll() {
+        return []
+    }
+
+    @Get(':id') // GET /users/:id
+    findOne(@Param('id') id: string) {
+        return { id }
+    }
+    
+    @Get('intern')  // GET /users/interns
+    findAllInterns() {
+        return []
+    }
+}
